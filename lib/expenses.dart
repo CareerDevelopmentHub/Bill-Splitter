1b8eaa89fb4d42c3271ed62a2ca1d771be4211a6
Priyanshu Mohanty <priyanshumohanty705@gmail.com>
2022-10-09 11:16:46 +0530

    Fixed the add expense button,added a settings button on each expense tile and aligned the expense button at the bottom

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,14 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'homepage.dart';
 import 'neubox.dart';
 
-class ExpenseTile extends StatelessWidget {
+class ExpenseTile extends StatefulWidget {
   final String groupname;
   final String des;
   final String Total;
   final String received;
+  final Function(BuildContext)? settings;
+  final Function(BuildContext)? onEdit;
+  final Function(BuildContext)? onDelete;
 
   const ExpenseTile({
     Key? key,
@@ -16,8 +22,16 @@ class ExpenseTile extends StatelessWidget {
     required this.des,
     required this.Total,
     required this.received,
+    required this.settings,
+    required this.onEdit,
+    required this.onDelete,
   }) : super(key: key);
 
+  @override
+  State<ExpenseTile> createState() => _ExpenseTileState();
+}
+
+class _ExpenseTileState extends State<ExpenseTile> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,7 +40,7 @@ class ExpenseTile extends StatelessWidget {
         child: GestureDetector(
           onTap: (() {
             Navigator.push(
-                context, MaterialPageRoute(builder: (_) => Homepage()));
+                context, MaterialPageRoute(builder: (_) => const Homepage()));
           }),
           child: NeuBox(
               child: Padding(
@@ -40,6 +54,7 @@ class ExpenseTile extends StatelessWidget {
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   // ignore: prefer_const_literals_to_create_immutables
                   children: [
                     Column(
@@ -47,15 +62,18 @@ class ExpenseTile extends StatelessWidget {
                       // ignore: prefer_const_literals_to_create_immutables
                       children: [
                         Text(
-                          groupname,
-                          style: TextStyle(
+                          widget.groupname,
+                          style: const TextStyle(
                               fontSize: 35,
                               fontWeight: FontWeight.bold,
                               color: Color.fromARGB(255, 18, 17, 17)),
                         ),
-                        Text(des),
+                        Text(widget.des),
                       ],
                     ),
+                    InkWell(
+                        onTap: () => widget.settings,
+                        child: const Icon(Icons.more_vert))
                   ],
                 ),
 
@@ -82,8 +100,8 @@ class ExpenseTile extends StatelessWidget {
                               color: Color.fromARGB(255, 8, 8, 8)),
                         ),
                         Text(
-                          Total,
-                          style: TextStyle(
+                          widget.Total,
+                          style: const TextStyle(
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
                               color: Color.fromARGB(255, 34, 34, 34)),
@@ -101,10 +119,10 @@ class ExpenseTile extends StatelessWidget {
                               color: Color.fromARGB(255, 66, 66, 66)),
                         ),
                         Text(
-                          received,
-                          style: TextStyle(
+                          widget.received,
+                          style: const TextStyle(
                               fontSize: 15,
-                              color: const Color.fromARGB(255, 66, 66, 66)),
+                              color: Color.fromARGB(255, 66, 66, 66)),
                         )
                       ],
                     ),
