25cc13714b13641730233907b3596aaaa0cd5c2d
Nayaneeka Mahapatra <mahapatranayaneeka@gmail.com>
2022-10-25 12:31:48 +0530

    Revert "Added Contact Picker"

--- lib/friends.dart
+++ lib/friends.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:contact_picker/contact_picker.dart';
 import 'package:flutter/material.dart';
 
 import 'neubox.dart';
@@ -22,9 +21,6 @@ class Friends extends StatefulWidget {
 }
 
 class _FriendsState extends State<Friends> {
-  final ContactPicker _contactPicker = new ContactPicker();
-  late Contact _contact;
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -85,18 +81,9 @@ class _FriendsState extends State<Friends> {
                             value: 7,
                             onChanged: ((value) {})),
                       ],
-                    ),
+                    )
                   ],
-                ),
-                GestureDetector(
-                  child: Icon(Icons.call),
-                  onTap: () async {
-                    Contact contact = await _contactPicker.selectContact();
-                    setState(() {
-                      _contact = contact;
-                    });
-                  },
-                ),
+                )
               ],
             ),
           ),
